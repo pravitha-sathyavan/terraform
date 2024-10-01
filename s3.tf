c675df9f2713c1b101503b33bd0c2f8de7e6f1da
Pravitha Sathyavan <46380586+pravitha-sathyavan@users.noreply.github.com>
2024-10-01 10:40:26 +0530

    Create s3.tf

---
+++ s3.tf
@@ -0,0 +1,30 @@
+
+# Step 1: Create an S3 Bucket
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "my-comprehensive-s3-bucket"
+}
+
+# Step 2: Set Bucket Policy (to allow public read access for a specific object prefix)
+resource "aws_s3_bucket_policy" "my_bucket_policy" {
+  bucket = aws_s3_bucket.my_bucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = ["s3:GetObject"],
+        Effect = "Allow", 
+        Resource = "${aws_s3_bucket.my_bucket.arn}/public/*", 
+        Principal = "*"
+      }
+    ]
+  })
+}
+
+# Resources: bucket and objects
+# Effect: Allow/Deny
+# Action: API like GetObject to allow/deny
+# Principal: The user or account to apply the policy to
+
+# If we want to give an EC2 instance access to S3 bucket, then create an EC2 instance role with correct IAM permissions and then EC2 instance will be able to access the S3 bucket.
+
